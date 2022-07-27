afd487d110ff8ef8c58209802a762ce866c09345
suchart <suchartsuttinak@gmail.com>
2022-07-27 15:49:14 +0700

    vdo1_success

---
+++ app/Http/Controllers/SiteController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SiteController extends Controller
+{
+    public function index(){
+        return view('welcome');
+    }
+    public function about(){
+        $company = 'CCT';
+        $users = ['John', 'Mary', 'Bob'];
+        return view('about',[
+            'company2' => $company,
+            'user' =>$users
+        ]);
+    }
+
+
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\SiteController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,10 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-})->name('welcome');
+Route::get('/', [SiteController::class, 'index'])->name('welcome');
 
-Route::get('/about', function () {
-    return "About Page";
-})->name('about');
+Route::get('/about', [SiteController::class, 'about'])->name('about');
